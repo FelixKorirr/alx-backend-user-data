6c0fb3511135e0a01513f4bb436126ee049ce228
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-23 17:33:32 +0300

    basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,11 +9,14 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = Auth()
+auth = None
+
+auth_type = getenv('AUTH_TYPE')
+if auth_type == 'auth':
+    auth = Auth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Implements basic authentication"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """Represents subclass basic auth"""
+    pass
