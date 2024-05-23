6f2e625e00343003b6427c1e9d1992749c3a419e
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-23 09:03:09 +0300

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,7 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Defines routes that don't need authentication"""
+        """Defines routes require or don't need authentication"""
         if path is None:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
@@ -20,8 +20,7 @@ class Auth:
         for item in excluded_paths:
             if path.rstrip('/') == item.rstrip('/'):
                 return False
-            else:
-                return True
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns none"""
