936ad3f21d13ea3d0a28c9b0fde6c56845c3c268
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-25 08:08:08 +0300

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -53,14 +53,16 @@ def forbidden(error) -> str:
 def before_request():
     """This function is executed before each request"""
     excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     request.current_user = auth.current_user(request)
 
     if auth is None:
         return
     if auth.require_auth(request.path, excluded_paths) is False:
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and \
+            auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
