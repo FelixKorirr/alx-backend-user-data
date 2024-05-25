2a172338358f9d0a4b761d0117d3aad671792b98
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-25 08:00:15 +0300

    session authentication

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -41,3 +41,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns none"""
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """Gets the value of the cookie named SESSION_NAME.
+        """
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
