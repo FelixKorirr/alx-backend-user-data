df918fcd627ed9b9040571df4491f00826a1cb46
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-29 23:34:22 +0300

    user_authentication_service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -23,23 +23,25 @@ def new_user():
     try:
         user_obj = AUTH.register_user(email, password)
         if user_obj:
-            return {"email": email, "message": "user created"}
+            return jsonify({"email": email, "message": "user created"})
     except ValueError:
-        return {"message": "email already registered"}, 400
-    
+        return jsonify({"message": "email already registered"}, 400)
+
+
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login():
+    """Responds to POST request to /sessions endpoint"""
     email = request.form.get('email')
     password = request.form.get('password')
 
     result = AUTH.valid_login(email, password)
     if result is True:
         session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
     else:
         abort(401)
-
-
-
 
 
 if __name__ == '__main__':
