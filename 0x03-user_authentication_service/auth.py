4df6741f25080d168bd2ba67f0f9915f2d2e9530
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-29 20:41:48 +0300

    user_authentication_service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -58,7 +58,7 @@ class Auth:
         """Creates session id"""
         try:
             usr = self._db.find_user_by(email=email)
-        except (AttributeError, NoResultFound):
+        except NoResultFound:
             return None
         this_session_id = _generate_uuid()
         self._db.update_user(usr.id, session_id=this_session_id)
