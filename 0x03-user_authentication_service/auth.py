052e41e3c4edd7bd85bf688c427d901415b99d25
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-29 22:36:58 +0300

    user_authentication_service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,11 +55,14 @@ class Auth:
             return False
 
     def create_session(self, email: str) -> str:
-        """Creates session id"""
+        """Creates a session for user corresponding to email
+        """
         try:
-            usr = self._db.find_user_by(email=email)
-            session_id = _generate_uuid()
-            self._db.update_user(usr.id, session_id=session_id)
-            return session_id
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                self._db.update_user(user.id, session_id=session_id)
+                return user.session_id
+            return None
         except NoResultFound:
             return None
