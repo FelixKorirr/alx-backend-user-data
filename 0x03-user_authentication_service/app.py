b4bc136c2b3e39964cab339846d719b642ef7c90
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-30 10:38:15 +0300

    user_authentication_service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,7 +49,7 @@ def logout() -> str:
     """Responds to DELETE /sessions route"""
     session_id = request.cookies.get('session_id')
     if session_id is None:
-        return abort(403)
+        abort(403)
 
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
