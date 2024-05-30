d2cdcd828db1e5ac2f767b38f5a2c3202fa1bdbe
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-30 12:21:08 +0300

    user_authentication_service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -68,5 +68,16 @@ def profile() -> str:
         abort(403)
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_token() -> str:
+    email = request.form.get('email')
+    user = AUTH._db.find_user_by(email=email)
+    if user is None:
+        abort(403)
+    reset_token = AUTH.get_reset_password_token(email)
+    AUTH._db.update_user(user.id, reset_token=reset_token)
+    return jsonify({"email": email, "reset_token": reset_token})
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
