ab61246593ef064b876edf8b9757886c8ea0e4f7
Felix Kipkorir <kipronofelix63@gmail.com>
2024-05-30 13:12:02 +0300

    user_authentication_service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,5 +79,19 @@ def get_reset_token() -> str:
     return jsonify({"email": email, "reset_token": reset_token})
 
 
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """Handles PUT requests to /reset_password endpoint.
+    Requests to update password"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    if reset_token is None:
+        abort(403)
+    AUTH.update_password(reset_token, new_password)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
